28f8306baeffac9317aa14dc08b1565ba3d6e5dd
Ian Lai <ian24257587@gmail.com>
2022-04-17 21:55:09 +0800

    feat: add routing buttons

--- client/src/components/layout/components/LeftPanel.tsx
+++ client/src/components/layout/components/LeftPanel.tsx
@@ -1,12 +1,12 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import Button from '@@components/ui/button/button';
-import { AddBoxOutlined, Explore, Mode } from '@mui/icons-material';
-
-export interface ILeftPanel {
-  
-}
+import { Explore } from '@mui/icons-material';
+import { useEthers } from '@usedapp/core';
 
 const LeftPanel = () => {
+  const { account } = useEthers();
+
   return (
     <div className="flex flex-col w-full h-full">
       <section className="flex justify-center items-center">
@@ -14,11 +14,21 @@ const LeftPanel = () => {
       </section>
       <section className="flex flex-col w-full h-full pl-3 items-center">
         <Button
+          className="mb-8"
           size="medium"
           startIcon={<Explore className="text-green-900 w-6 h-6" />}
         >
-          <span className="text-green-900	text-xl font-bold ml-12">發現</span>
+          <Link href="/" passHref>
+            <span className="text-green-900	text-xl font-bold ml-12">發現</span>
+          </Link>
         </Button>
+        {!!account && (
+          <Button bgColor="primary">
+            <Link href="/drafts" passHref>
+              <span className="text-white font-bold">創作</span>
+            </Link>
+          </Button>
+        )}
       </section>
     </div>
   );
--- client/src/pages/connect-wallet/index.tsx
+++ client/src/pages/connect-wallet/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useEthers } from '@usedapp/core';
 import ConnectWalletComponent from '@@components/connectWalletPage';
 
 const ConnectWallet = () => {
+  const { account } = useEthers();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!!account) {
+      router.replace('/');
+    }
+  }, [account, router]);
+
   return (
     <ConnectWalletComponent />
   );
