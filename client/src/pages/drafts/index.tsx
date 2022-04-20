f3ba8bccbae3f2341673efa6f6fc8686db494eb1
Ian Lai <ian24257587@gmail.com>
2022-04-21 00:44:58 +0800

    feat: build mutation update query cache

--- client/src/pages/drafts/index.tsx
+++ client/src/pages/drafts/index.tsx
@@ -5,11 +5,53 @@ import DraftsComponent from '@@components/draftsPage';
 import type { IArticleInput } from '@@common/article';
 import { useSnackbar } from '@@components/snackbar';
 import { ADD_ARTICLE_MUTATION } from '../../api/graphQL';
+import { ARTICLE_LIST_QUERY, ARTICLE_AMOUNT_QUERY } from '../../api/graphQL';
+import { AMOUNT_PER_PAGE } from '@@constants/pagination';
+import { QueriedArticleCount, QueriedArticleList } from '@@interfaces/index';
 
 const Drafts = () => {
   const router = useRouter();
   const [, setModel] = useSnackbar();
-  const [addArticle, { loading, error }] = useMutation<IArticleInput>(ADD_ARTICLE_MUTATION);
+  const [addArticle, { loading, error }] = useMutation<{ addArticle: IArticleInput}>(ADD_ARTICLE_MUTATION, {
+    update(cache, { data }) {
+      const cacheCount = cache.readQuery<QueriedArticleCount>({
+        query: ARTICLE_AMOUNT_QUERY,
+      });
+
+      const offset = (Math.ceil(cacheCount.articlesCount / AMOUNT_PER_PAGE) - 1) * AMOUNT_PER_PAGE;
+
+      const cacheArticles = cache.readQuery<QueriedArticleList>({
+        query: ARTICLE_LIST_QUERY,
+        variables: {
+          offset,
+          limit: AMOUNT_PER_PAGE,
+        },
+      });
+
+      if (cacheArticles.articles.length === AMOUNT_PER_PAGE) return;
+
+      console.log(cacheCount, cacheArticles);
+
+      cache.writeQuery({
+        query: ARTICLE_LIST_QUERY,
+        variables: {
+          offset,
+          limit: AMOUNT_PER_PAGE,
+        },
+        data: {
+          articles: [
+            ...cacheArticles.articles,
+            {
+              ...data.addArticle,
+            },
+          ],
+        },
+      });
+    },
+    refetchQueries: [{
+      query: ARTICLE_AMOUNT_QUERY,
+    }],
+  });
 
   return (
     (loading || error) ? (
